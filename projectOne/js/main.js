238eb66fd6330c1f9aabc1b02105b77412c6bfbe
Jean Menezes <jottamenezes@gmail.com>
2019-04-29 15:01:06 -0700

    Merging to iMac

--- projectOne/js/main.js
+++ projectOne/js/main.js
@@ -3,11 +3,22 @@
 var img1 = document.querySelector('#img1');
 var arrowLeft = document.querySelector('#arrowLeft');
 var closeButton = document.querySelector('#closeButton');
+var windowW;
 
-arrowLeft.addEventListener('hover', function() {
+window.addEventListener('resize', getWindowWidth);
+
+function getWindowWidth() {
+  windowW = window.innerWidth;
+  console.log('The Viewport is : ' + windowW);
+
+}
+
+
+
+arrowLeft.addEventListener('mouseover', function() {
   // console.log('mousedown on button');
   img1.style.opacity = "0.2";
-  img1.style.transform = "rotate(5deg)";
+  img1.style.transform = "rotate(-5deg)";
 
 
 });
