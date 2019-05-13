310306abe1d28545b1bdd38a617943f2b7624591
JeanMenezes <jottamenezes@gmail.com>
2019-05-13 12:15:14 -0700

    PosetoPose finished

---
+++ Animation-Principles/principle_four/js/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+
+var windowW;
+
+window.addEventListener('resize', getWindowWidth);
+
+function getWindowWidth() {
+  windowW = window.innerWidth;
+  console.log('The Viewport is : ' + windowW);
+
+}
+
+
+//
+// arrowLeft.addEventListener('mouseover', function() {
+//   // console.log('mousedown on button');
+//   img1.style.opacity = "0.2";
+//   img1.style.transform = "rotate(-5deg)", "scale(0.5)";
+//
+//
+//
+// });
+//
+// arrowLeft.addEventListener('mouseout', function() {
+//   // console.log('mousedown on button');
+//   img1.style.opacity = "1";
+//   img1.style.transform = "rotate(0deg)";
+//
+//
+//
+//
+//
+// });
